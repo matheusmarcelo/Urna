84ff744723b73deb1e6ae800ca5a85f671fe2c89
Matheus Marcelo Simões Neves <matheusmarcelo314@gmail.com>
2021-07-29 17:18:12 -0300

    Primeiro Commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+                {
+                    numero: '38111',
+                    nome : 'Cebolinha',
+                    partido: 'ABC',
+                    foto: [
+                        {url: 'cebolinha.png', legenda: 'Vereador', small: false}
+                    ]
+                },
+                {
+                    numero: '77222' ,
+                    nome: 'Bart Simpson',
+                    partido: 'DEFG',
+                    foto: [
+                        {url: 'bart.jpg', legenda: 'Vereador'}
+                    ]
+                },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Goku',
+                partido: 'ABC',
+                vice: 'Vegeta',
+                foto: [
+                    {url: 'goku.jpg', legenda: 'Prefeito'},
+                    {url: 'vegeta.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Naruto',
+                partido: 'QWERTY',
+                vice: 'Sasuke',
+                foto: [
+                    {url: 'naruto.jpg', legenda: 'Prefeito'},
+                    {url: 'sasuke.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
